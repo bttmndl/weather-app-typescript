b301e39c1f3d8b8549ee237fa3d853f89f1f4150
bttmndl <bttmndl@gmail.com>
2023-02-18 20:07:25 +0530

    intial api setup done

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 interface Props {
   inputValue : string,
 }
 
-const MainContent:React.FC <Props>= ({inputValue}) => {
+interface Country {
+  capital: string;
+  population: number;
+  latlng: string,
+  flag: string;
+}
 
+const MainContent:React.FC <Props>= ({inputValue}) => {
+  const [weatherCountryData, setWeatherCountryData] = useState<Country[]>();
+
+  useEffect(() => {
+    async function fetchData(): Promise<void> {
+      const response = await axios.get<Country[]>(`https://restcountries.com/v3.1/name/${inputValue}`);
+      setWeatherCountryData(response.data);
+    }
+
+    if(inputValue) fetchData();
+  }, [inputValue]);
+
+  console.log(weatherCountryData)
 
   return (
-    <div>MainContent</div>
+    <div className='main_content'>
+      {
+        weatherCountryData && weatherCountryData.map((ele, idx)=>(
+          <div className='main_content_inner'>
+            <h1>{ele.capital}</h1>
+            <h2>{ele.population}</h2>
+            <h2>{ele.latlng}</h2>
+            <h2>{ele.flag}</h2>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
